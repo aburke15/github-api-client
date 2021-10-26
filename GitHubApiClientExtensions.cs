25266553f070e5a8dd731340f2d454a54608e677
Andre Burke <aburke.dev@gmail.com>
2021-10-25 19:22:03 -0600

    initial commit

---
+++ GitHubApiClientExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GitHubApiClient
+{
+    public static class GitHubApiClientExtensions
+    {
+        public static IServiceCollection AddGitHubApiClient(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
